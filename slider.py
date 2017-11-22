3ce5c29b93e87ffa2ff0b11b552ff31a448d13b4
ojhall94 <ojh251@student.bham.ac.uk>
2017-11-22 16:50:31 +0000

    Added histogram update to slider.py, not yet fully functional

--- slider.py
+++ slider.py
@@ -22,36 +22,44 @@ class cUpdate:
         return (out.M_ks.values, out.Ks.values)
 
 if __name__ == "__main__":
+    plt.close('all')
     sfile = glob.glob('../data/TRILEGAL_sim/*all*.txt')[0]
     dff = pd.read_csv(sfile, sep='\s+')
 
+    '''Correct data'''
     dff['Aks'] = 0.114*dff.Av
     dff['M_ks'] = dff.Ks - dff['m-M0'] - dff.Aks
 
+    '''Set first order cuts on data'''
     dff = dff[dff.M_ks < -0.5]
     dff = dff[dff.M_ks > -2.5]
     dff = dff[dff.Ks < 15.]
     dff = dff[dff.Ks > 6.]
-
     dff = dff[0:8000]
+
+    '''Load in first order data'''
     U = cUpdate(dff)
 
+    '''Set second order cuts on data'''
     loggmaxvalinit = dff.logg.max()
     loggminvalinit = dff.logg.min()
     zmaxvalinit = 0.5
     zminvalinit = -0.5
 
+    '''Define initial subsample of second order data'''
     init = dff[dff.logg < loggmaxvalinit]
     init = init[init.logg > loggminvalinit]
     init = init[init['[M/H]'] < zmaxvalinit]
     init = init[init['[M/H]'] > zminvalinit]
+
+    '''Define plottable variables of first subsample'''
     x = init.M_ks
     y = init.Ks
     cg = init.logg
     cm = init['[M/H]']
 
+    '''Initiate plots'''
     fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True,sharey=True)
-    plt.subplots_adjust(bottom=0.35)
     l = ax1.scatter(x, y, cmap='cool', c=cg, s=3)
     m = ax2.scatter(x, y, cmap='Oranges', c=cm, s=3)
     fig.colorbar(l, ax=ax1, label=r'log_{10}(g)')
@@ -65,17 +73,26 @@ if __name__ == "__main__":
     ax2.set_axisbelow(True)
     fig.suptitle(r"TRILEGAL simulated data at magnitudes near the Red Clump")
 
+    '''Initiate sliders'''
+    Sfig, Sax = plt.subplots()
+    Sfig.subplots_adjust(bottom=0.45)
+    h = Sax.hist(x,histtype='step',color='k',bins=int(np.sqrt(len(x))))
+    Sax.set_xlabel(r"$M_{Ks}$")
+    Sax.set_ylabel('Counts')
+    Sax.set_title(r"Histogram in Absolute Magnitude for TRILGAL sample")
+
     axcolor = 'white'
-    axloggmax = plt.axes([0.15, 0.05, 0.60, 0.03], facecolor=axcolor)
-    axloggmin = plt.axes([0.15, 0.1, 0.60, 0.03], facecolor=axcolor)
-    axzmax = plt.axes([0.15, 0.15, 0.60, 0.03], facecolor=axcolor)
-    axzmin = plt.axes([0.15, 0.20, 0.60, 0.03], facecolor=axcolor)
+    axloggmax = Sfig.add_axes([0.15, 0.05, 0.60, 0.03], facecolor=axcolor)
+    axloggmin = Sfig.add_axes([0.15, 0.1, 0.60, 0.03], facecolor=axcolor)
+    axzmax = Sfig.add_axes([0.15, 0.15, 0.60, 0.03], facecolor=axcolor)
+    axzmin = Sfig.add_axes([0.15, 0.20, 0.60, 0.03], facecolor=axcolor)
 
     sloggmax = Slider(axloggmax, 'Max log(g)', round(dff.logg.min()), round(dff.logg.max()),valinit = loggmaxvalinit)
     sloggmin = Slider(axloggmin, 'Min log(g)', round(dff.logg.min()), round(dff.logg.max()),valinit = loggminvalinit)
     szmax = Slider(axzmax, 'Max [M/H]', round(dff['[M/H]'].min()), round(dff['[M/H]'].max()),valinit = zmaxvalinit)
     szmin = Slider(axzmin, 'Min [M/H]', round(dff['[M/H]'].min()), round(dff['[M/H]'].max()),valinit = zminvalinit)
 
+    '''Update function'''
     def update(val):
         loggmax = sloggmax.val
         loggmin = sloggmin.val
@@ -85,27 +102,30 @@ if __name__ == "__main__":
         uu = np.vstack(U.get_update(loggmin, loggmax, zmin, zmax))
         l.set_offsets(uu.T)
         m.set_offsets(uu.T)
+        Sax.cla()
+        Sax.hist(uu[0],histtype='step',color='k',bins=int(np.sqrt(len(uu[0]))))
 
         l.set_clim([loggmin,loggmax])
         m.set_clim([zmin,zmax])
         fig.canvas.draw_idle()
+        Sfig.canvas.draw_idle()
 
+    '''Calling updates on the second order data'''
     sloggmax.on_changed(update)
     sloggmin.on_changed(update)
     szmax.on_changed(update)
     szmin.on_changed(update)
 
+    '''Initiating reset buttons'''
     resetax = plt.axes([0.83, 0.025, 0.15, 0.04])
     zresetax = plt.axes([0.83, 0.125, 0.15, 0.04])
     gresetax = plt.axes([0.83, 0.225, 0.15, 0.04])
-
 
     buttonz = Button(zresetax, 'Reset [M/H]', color=axcolor, hovercolor='0.975')
     buttong = Button(gresetax, 'Reset log(g)', color=axcolor, hovercolor='0.975')
     button  = Button(resetax, 'Reset all', color=axcolor, hovercolor='0.975')
 
-
-
+    '''Reset functions'''
     def reset(event):
         sloggmax.reset()
         sloggmin.reset()
