eaeed9823293757c6fa51abeb3a318932f8e2490
ojhall94 <ojh251@student.bham.ac.uk>
2017-11-27 15:24:19 +0000

    Slider is now fully functional. Relevant data is contained in repo.

--- slider.py
+++ slider.py
@@ -9,26 +9,109 @@ import matplotlib.mlab as mlab
 from matplotlib.widgets import Slider, Button, RadioButtons
 import glob as glob
 import pandas as pd
+import datetime
+import os
+
+
+def save(event):
+    loggmax = sloggmax.val
+    loggmin = sloggmin.val
+    zmax = szmax.val
+    zmin = szmin.val
+    cmax = scolmax.val
+    cmin = scolmin.val
+    out = U.get_update(loggmin, loggmax, zmin, zmax, cmin, cmax, save=True)
+
+    '''Setting up extra-repo output space'''
+    if os.path.isdir('../Cuts_Data') == False:
+        os.mkdir('../Cuts_Data')
+
+    '''Saving data with cuts and date of file in filename'''
+    date = datetime.datetime.now()
+    out.to_csv('../Cuts_Data/cuts_MH_JKs_logg.txt', sep='\t')
+
+    cuts = pd.DataFrame(columns=['Logg_Min','Logg_Max','[M/H]_Min','[M/H]_Max',\
+                                'J-Ks_Min','J-Ks_Max'])
+    cuts.loc[0] = [loggmin, loggmax, zmin, zmax, cmin, cmax]
+    cuts.to_csv('../Cuts_Data/cuts_'+str(date.year)+str(date.month)+str(date.day)+'.txt', sep='\t')
+
+    Lfig.savefig('../Cuts_Data/cut_losses_'+str(date.year)+str(date.month)+str(date.day)+'.png')
 
 class cUpdate:
     def __init__(self,_dff):
         self.dff = _dff
 
-    def get_update(self,loggmin, loggmax, zmin, zmax):
+    def get_update(self,loggmin, loggmax, zmin, zmax, cmin, cmax, save=False):
         out = self.dff[dff.logg < loggmax]
         out = out[out.logg > loggmin]
         out = out[out['[M/H]'] < zmax]
         out = out[out['[M/H]'] > zmin]
-        return (out.M_ks.values, out.Ks.values)
+        out = out[out.JKs < cmax]
+        out = out[out.JKs > cmin]
+
+        if save:
+            return out
+        rc_new = len(out[out.stage==4])
+        rgb_new = len(out[out.stage==3])
+        alt_new = len(out[(out.stage != 3) & (out.stage != 4)])
+
+        return np.vstack((out.M_ks.values, out.Ks.values)), (rc_new, rgb_new, alt_new)
+
+'''Update function'''
+def update(val):
+    loggmax = sloggmax.val
+    loggmin = sloggmin.val
+    zmax = szmax.val
+    zmin = szmin.val
+    cmax = scolmax.val
+    cmin = scolmin.val
+
+    uu, ss = U.get_update(loggmin, loggmax, zmin, zmax, cmin, cmax)
+    '''Update Plots'''
+    l.set_offsets(uu.T)
+    m.set_offsets(uu.T)
+    c.set_offsets(uu.T)
+    l.set_clim([loggmin,loggmax])
+    m.set_clim([zmin,zmax])
+    c.set_clim([cmin,cmax])
+
+    '''Update Histogram'''
+    Sax.clear()
+    Sax.hist(init.M_ks,histtype='step',color='r',bins=int(np.sqrt(len(init.M_ks))),label='Histogram before cuts')
+    Sax.legend(loc='best',fancybox=True)
+    Sax.set_xlabel(r"$M_{Ks}$")
+    Sax.set_ylabel('Counts')
+    Sax.set_title(r"Histogram in Absolute Magnitude for TRILGAL sample")
+    Sax.hist(uu[0],histtype='step',color='k',bins=int(np.sqrt(len(uu[0]))),label='Histogram after cuts')
+
+    '''Update List'''
+    RC_n.set_text('Total after cut: '+str(ss[0]))
+    RC_p.set_text('Remaining: '+str(ss[0]*100/rc_total)+r"\%")
+    RGB_n.set_text('Total after cut: '+str(ss[1]))
+    RGB_p.set_text('Remaining: '+str(ss[1]*100/rgb_total)+r"\%")
+    ALT_n.set_text('Total after cut: '+str(ss[2]))
+
+    '''Draw Plots'''
+    Lfig.canvas.draw_idle()
+    Gfig.canvas.draw_idle()
+    Mfig.canvas.draw_idle()
+    Cfig.canvas.draw_idle()
+    Sfig.canvas.draw_idle()
+
+'''TODO:
+-Add cuts to text plot
+-Force save of full dataset
+'''
 
 if __name__ == "__main__":
     plt.close('all')
-    sfile = glob.glob('../data/TRILEGAL_sim/*all*.txt')[0]
+    sfile = glob.glob('Repo_Data/*all*.txt')[0]
     dff = pd.read_csv(sfile, sep='\s+')
 
     '''Correct data'''
     dff['Aks'] = 0.114*dff.Av
     dff['M_ks'] = dff.Ks - dff['m-M0'] - dff.Aks
+    dff['JKs'] = dff.J - dff.Ks
 
     '''Set first order cuts on data'''
     dff = dff[dff.M_ks < -0.5]
@@ -45,107 +128,155 @@ if __name__ == "__main__":
     loggminvalinit = dff.logg.min()
     zmaxvalinit = 0.5
     zminvalinit = -0.5
+    colmaxvalinit = dff.JKs.max()
+    colminvalinit = dff.JKs.min()
 
     '''Define initial subsample of second order data'''
     init = dff[dff.logg < loggmaxvalinit]
     init = init[init.logg > loggminvalinit]
     init = init[init['[M/H]'] < zmaxvalinit]
     init = init[init['[M/H]'] > zminvalinit]
+    init = init[init.JKs < colmaxvalinit]
+    init = init[init.JKs > colminvalinit]
+
+    '''Setting up stage contribution percentages'''
+    rc_total = len(dff[dff.stage == 4])
+    rgb_total = len(dff[dff.stage ==3])
+    alt_total = len(dff[(dff.stage != 3) & (dff.stage != 4)])
+    rc_init = len(init[init.stage==4])
+    rgb_init = len(init[init.stage==3])
+    alt_init = len(init[(init.stage != 3) & (init.stage != 4)])
+
+    rc_perc = rc_init*100/rc_total
+    rgb_perc = rgb_init*100/rgb_total
+
+    Lfig, Lax = plt.subplots(figsize=(2,4.5))
+    Lax.get_xaxis().set_visible(False)
+    Lax.get_yaxis().set_visible(False)
+
+    rc_txt = 'Remaining: '+str(rc_perc)+r"\%"
+    rgb_txt = 'Remaining: '+str(rgb_perc)+r"\%"
+    Lax.text(0.1, 0.9, b'Red Clump Stars:', color='red')
+    Lax.text(0.1, 0.85, 'Initial total: '+str(rc_total))
+    RC_n = Lax.text(0.1, 0.80, 'Total after cut: '+str(rc_init))
+    RC_p = Lax.text(0.1,0.75,rc_txt)
+
+    Lax.text(0.1, 0.60, b'RGB Stars:',color='red')
+    Lax.text(0.1, 0.55, 'Initial total: '+str(rgb_total))
+    RGB_n = Lax.text(0.1, 0.50, 'Total after cut: '+str(rgb_init))
+    RGB_p = Lax.text(0.1,0.45,rgb_txt)
+
+    Lax.text(0.1, 0.30, b'Other types:', color='red')
+    Lax.text(0.1, 0.25, 'Initial total: '+str(alt_total))
+    ALT_n = Lax.text(0.1, 0.20, 'Total after cut: '+str(alt_init))
 
     '''Define plottable variables of first subsample'''
     x = init.M_ks
     y = init.Ks
     cg = init.logg
     cm = init['[M/H]']
+    cc = init.JKs
 
     '''Initiate plots'''
-    fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True,sharey=True)
-    l = ax1.scatter(x, y, cmap='cool', c=cg, s=3)
-    m = ax2.scatter(x, y, cmap='Oranges', c=cm, s=3)
-    fig.colorbar(l, ax=ax1, label=r'log_{10}(g)')
-    fig.colorbar(m, ax=ax2, label='[M/H]')
+    Gfig, Gax = plt.subplots()
+    Mfig, Max = plt.subplots()
+    Cfig, Cax = plt.subplots()
+    l = Gax.scatter(x, y, cmap='cool', c=cg, s=3)
+    m = Max.scatter(x, y, cmap='winter', c=cm, s=3)
+    c = Cax.scatter(x, y, cmap='plasma', c=cc, s=3)
+    Gfig.colorbar(l, label=r"$log_{10}$(g)")
+    Mfig.colorbar(m, label='[M/H]')
+    Cfig.colorbar(c, label=r"J-$K_s$")
 
-    ax2.set_xlabel(r"$M_{Ks}$")
-    ax1.set_ylabel(r"$m_{Ks}$")
-    ax1.grid()
-    ax2.grid()
-    ax1.set_axisbelow(True)
-    ax2.set_axisbelow(True)
-    fig.suptitle(r"TRILEGAL simulated data at magnitudes near the Red Clump")
+    Gax.set_xlabel(r"$M_{Ks}$")
+    Max.set_xlabel(r"$M_{Ks}$")
+    Cax.set_xlabel(r"$M_{Ks}$")
+    Gax.set_ylabel(r"$m_{Ks}$")
+    Max.set_ylabel(r"$m_{Ks}$")
+    Cax.set_ylabel(r"$m_{Ks}$")
+
+    Gax.grid()
+    Max.grid()
+    Cax.grid()
+    Gax.set_axisbelow(True)
+    Max.set_axisbelow(True)
+    Cax.set_axisbelow(True)
+    Gfig.suptitle(r"TRILEGAL simulated data at magnitudes near the Red Clump (log10(g))")
+    Mfig.suptitle(r"TRILEGAL simulated data at magnitudes near the Red Clump ([M/H])")
+    Cfig.suptitle(r"TRILEGAL simulated data at magnitudes near the Red Clump (J-Ks)")
 
     '''Initiate sliders'''
     Sfig, Sax = plt.subplots()
     Sfig.subplots_adjust(bottom=0.45)
-    hist, bins = np.histogram(x,bins=int(np.sqrt(len(x))))
-    b = Sax.bar(bins[:-1],hist,color='k')
-    # _,_,h = Sax.hist(x,histtype='step',color='k',bins=int(np.sqrt(len(x))))
+    _,_,h = Sax.hist(x,histtype='step',color='k',bins=int(np.sqrt(len(x))))
     Sax.set_xlabel(r"$M_{Ks}$")
     Sax.set_ylabel('Counts')
     Sax.set_title(r"Histogram in Absolute Magnitude for TRILGAL sample")
 
     axcolor = 'white'
-    axloggmax = Sfig.add_axes([0.15, 0.05, 0.60, 0.03], facecolor=axcolor)
-    axloggmin = Sfig.add_axes([0.15, 0.1, 0.60, 0.03], facecolor=axcolor)
-    axzmax = Sfig.add_axes([0.15, 0.15, 0.60, 0.03], facecolor=axcolor)
-    axzmin = Sfig.add_axes([0.15, 0.20, 0.60, 0.03], facecolor=axcolor)
+    axloggmax = Sfig.add_axes([0.15, 0.1, 0.60, 0.03], facecolor=axcolor)
+    axloggmin = Sfig.add_axes([0.15, 0.14, 0.60, 0.03], facecolor=axcolor)
+    axzmax = Sfig.add_axes([0.15, 0.18, 0.60, 0.03], facecolor=axcolor)
+    axzmin = Sfig.add_axes([0.15, 0.22, 0.60, 0.03], facecolor=axcolor)
+    axcolmax = Sfig.add_axes([0.15, 0.26, 0.60, 0.03], facecolor=axcolor)
+    axcolmin = Sfig.add_axes([0.15, 0.30, 0.60, 0.03], facecolor=axcolor)
 
     sloggmax = Slider(axloggmax, 'Max log(g)', round(dff.logg.min()), round(dff.logg.max()),valinit = loggmaxvalinit)
     sloggmin = Slider(axloggmin, 'Min log(g)', round(dff.logg.min()), round(dff.logg.max()),valinit = loggminvalinit)
     szmax = Slider(axzmax, 'Max [M/H]', round(dff['[M/H]'].min()), round(dff['[M/H]'].max()),valinit = zmaxvalinit)
     szmin = Slider(axzmin, 'Min [M/H]', round(dff['[M/H]'].min()), round(dff['[M/H]'].max()),valinit = zminvalinit)
-
-    '''Update function'''
-    def update(val):
-        loggmax = sloggmax.val
-        loggmin = sloggmin.val
-        zmax = szmax.val
-        zmin = szmin.val
-
-        uu = np.vstack(U.get_update(loggmin, loggmax, zmin, zmax))
-        l.set_offsets(uu.T)
-        m.set_offsets(uu.T)
-        hist, bins = np.histogram(uu[0],bins=int(np.sqrt(len(uu[0]))))
-        [bar.set_height(hist[i]) for i, bar in enumerate(b)]
-        [bar.set_x(bins[i]) for i, bar in enumerate(b)]
-        Sax.relim()
-        Sax.autoscale_view()
-        # Sax.cla()
-        # Sax.hist(uu[0],histtype='step',color='k',bins=int(np.sqrt(len(uu[0]))))
-
-        l.set_clim([loggmin,loggmax])
-        m.set_clim([zmin,zmax])
-        fig.canvas.draw_idle()
-        Sfig.canvas.draw_idle()
+    scolmax = Slider(axcolmax, 'Max J-Ks', round(dff['JKs'].min()), round(dff['JKs'].max()),valinit = colmaxvalinit)
+    scolmin = Slider(axcolmin, 'Min J-Ks', round(dff['JKs'].min()), round(dff['JKs'].max()),valinit = colminvalinit)
 
     '''Calling updates on the second order data'''
     sloggmax.on_changed(update)
     sloggmin.on_changed(update)
     szmax.on_changed(update)
     szmin.on_changed(update)
+    scolmax.on_changed(update)
+    scolmin.on_changed(update)
 
     '''Initiating reset buttons'''
-    resetax = plt.axes([0.83, 0.025, 0.15, 0.04])
-    zresetax = plt.axes([0.83, 0.125, 0.15, 0.04])
-    gresetax = plt.axes([0.83, 0.225, 0.15, 0.04])
+    resetax = plt.axes([0.83, 0.16, 0.15, 0.04])
+    zresetax = plt.axes([0.83, 0.21, 0.15, 0.04])
+    gresetax = plt.axes([0.83, 0.26, 0.15, 0.04])
+    cresetax = plt.axes([0.83, 0.31, 0.15, 0.04])
 
-    buttonz = Button(zresetax, 'Reset [M/H]', color=axcolor, hovercolor='0.975')
-    buttong = Button(gresetax, 'Reset log(g)', color=axcolor, hovercolor='0.975')
-    button  = Button(resetax, 'Reset all', color=axcolor, hovercolor='0.975')
+    saveax = plt.axes([0.15, 0.04, 0.83, 0.05])
+    closeax = plt.axes([0.83, 0.10, 0.15, 0.04])
 
+    buttonz = Button(zresetax, 'Reset [M/H]', color=axcolor, hovercolor='0.7')
+    buttong = Button(gresetax, 'Reset log(g)', color=axcolor, hovercolor='0.7')
+    buttonc = Button(cresetax, 'Reset J-Ks', color=axcolor, hovercolor='0.7')
+    button  = Button(resetax, 'Reset all', color=axcolor, hovercolor='0.7')
+
+    buttonsave = Button(saveax, 'Save Cut Data (with list of cuts)',color=axcolor, hovercolor='.7')
+    buttonclose = Button(closeax, 'Close Plots', color=axcolor, hovercolor='red')
     '''Reset functions'''
     def reset(event):
         sloggmax.reset()
         sloggmin.reset()
         szmax.reset()
         szmin.reset()
+        scolmax.reset()
+        scolmin.reset()
     def resetg(event):
         sloggmax.reset()
         sloggmin.reset()
     def resetz(event):
         szmax.reset()
         szmin.reset()
+    def resetc(event):
+        scolmax.reset()
+        scolmin.reset()
+    def close(event):
+        plt.close('all')
+
     buttonz.on_clicked(resetz)
     buttong.on_clicked(resetg)
+    buttonc.on_clicked(resetc)
     button.on_clicked(reset)
+    buttonsave.on_clicked(save)
+    buttonclose.on_clicked(close)
 
-    plt.show()
+    plt.show(block=False)
