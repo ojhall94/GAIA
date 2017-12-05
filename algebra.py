9870f461ff107732416841be8e5a956ce7dab7dc
ojhall94 <ojh251@student.bham.ac.uk>
2017-12-05 14:02:24 +0000

    Included algebra workbench for sympy testing of renormalization on foreground model for RGBB.py

---
+++ algebra.py
@@ -0,0 +1,34 @@
+from sympy import *
+import numpy as np
+
+
+if __name__ == '__main__':
+    xlo = 0.795676986895
+    xhi = 2.44879973203
+    ylo = 4171.36199658
+    yhi = 5160.89190244
+
+    labels_mc = ["$m$", "$c$", r"$\sigma(m)$","$lam$"]
+    p = np.array([4.60535762e+02, 3.92085724e+03, 6.98355734e+01 , 1.])
+
+    init_printing(use_unicode=False, wrap_line=False, no_global=True)
+    x, y, c, m, lam, sig = symbols(['x', 'y', 'c', 'm','lam', 'sig'])
+
+    fn = exp(lam*x)
+    Ix = integrate(fn, (x, xlo, xhi))
+
+    Intx = lambdify((lam), Ix)
+    A = 1/Intx(p[-1])
+
+    # fn = -0.5*((y - c - m*x)/sig)**2# + lam*x
+    # Iy = integrate(exp(fn), (y,ylo,yhi))#, (y,ylo,yhi))
+
+    # print(I)
+    #
+    # Int = lambdify((c, m, lam, sig), I)
+    #
+    #
+    # C = Int(*p)
+    #
+    #
+    # print(integrate(c*x*y, (x,1,2),(y,2,3)))
