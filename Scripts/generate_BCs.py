51a57e0f51487294f045d09694986c6040eb0aae
Oliver Hall <ojh251@student.bham.ac.uk>
2018-08-29 10:00:40 +0100

    Generated BCs properly, run some tests on both those and the updated omnitool scaling relations.

--- Scripts/generate_BCs.py
+++ Scripts/generate_BCs.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+pd.options.mode.chained_assignment = None  #Turn off SettingWithCopyWarning
 import os
 import omnitool
 
@@ -11,6 +12,8 @@ parser.add_argument('stage', type=str, choices=['load','unload'],
                     help='Load prepares the data for BCcodes. Unload saves it to a location of choice.')
 parser.add_argument('-pl', '--perturb_logg', action='store_const', const=True, default=False,
                     help='If true, perturb our value of logg using seismic scaling relations for the perturbed Teff')
+parser.add_argument('-r', '--reddening', action='store_const', const=True, default=False,
+                    help='If true, include reddening in the interpolation. WARNING: This is *not* required for the Hall+18 work.')
 args = parser.parse_args()
 
 __datadir__ = os.path.expanduser('~')+'/PhD/Gaia_Project/data/KepxDR2/'
@@ -19,7 +22,7 @@ __bccodes__ = os.path.expanduser('~')+'/PhD/Hacks_and_Mocks/bolometric-correctio
 if __name__ == '__main__':
     if args.stage == 'load':
         cdf = pd.read_csv(__datadir__+'rcxyu18_pre_elsworth.csv')
-        out = cdf[['KICID','[Fe/H]','Ebv']]    #Load in fixed values
+        out = cdf[['KICID','[Fe/H]']]   #Load in fixed values
         out['Teff'] = cdf['Teff'] + args.tempdiff   #Add temperature perturbation
 
         if not args.perturb_logg:
@@ -27,6 +30,11 @@ if __name__ == '__main__':
         else:
             sc = omnitool.scalings(cdf.numax, cdf.dnu, out.Teff)
             out['logg'] = sc.get_logg()
+
+        if not args.reddening:
+            out['Ebv'] = np.zeros(len(out))
+        else:
+            out['Ebv'] = cdf['Ebv']
 
         out = out[['KICID','logg','[Fe/H]','Teff','Ebv']]
 
--- Scripts/generate_BCs.sh
+++ Scripts/generate_BCs.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-'''
-Recalculate Bolometric Corrections for a given temperature perturbation
-
-positional arguments:
-  tempdiff             Perturbation to the temperature values in K
-  {load,unload}        Load prepares the data for BCcodes. Unload saves it to
-                       a location of choice.
-
-optional arguments:
-  -h, --help           show this help message and exit
-  -pl, --perturb_logg  If true, perturb our value of logg using seismic
-                       scaling relations for the perturbed Teff
-'''
+# Recalculate Bolometric Corrections for a given temperature perturbation
+#
+# positional arguments:
+#   tempdiff             Perturbation to the temperature values in K
+#   {load,unload}        Load prepares the data for BCcodes. Unload saves it to
+#                        a location of choice.
+#
+# optional arguments:
+#   -h, --help           show this help message and exit
+#   -pl, --perturb_logg  If true, perturb our value of logg using seismic
+#                        scaling relations for the perturbed Teff
+#   -r, --reddening      If true, include reddening in the interpolation.
+#                        WARNING: This is *not* required for the Hall+18 work.
 
 #Generate iteratively for a range of temperature offsets
 for i in {-50..50..10}; do
